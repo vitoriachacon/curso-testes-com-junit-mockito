1000be5b861a72ea54c7cf82fa285865f694721f
vitoriachacon <vitoria.chacon@hotmail.com>
2024-10-18 20:20:58 -0300

    inicialização do projeto

---
+++ src/main/java/br/com/chacon/ApiTestsJunitMockitoApplication.java
@@ -0,0 +1,13 @@
+package br.com.chacon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTestsJunitMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTestsJunitMockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/chacon/ApiTestsJunitMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.chacon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestsJunitMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
