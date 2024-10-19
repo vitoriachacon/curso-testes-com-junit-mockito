b83cfe4e1ec1a2137786a2e5900c7bd5ed1ec287
vitoriachacon <vitoria.chacon@hotmail.com>
2024-10-18 22:31:02 -0300

    Disponibilizando modelMapper em forma de bin

--- src/main/java/br/com/chacon/resources/UserResource.java
+++ src/main/java/br/com/chacon/resources/UserResource.java
@@ -1,7 +1,8 @@
 package br.com.chacon.resources;
 
-import br.com.chacon.domain.User;
+import br.com.chacon.domain.dto.UserDTO;
 import br.com.chacon.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,11 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserResource {
 
     @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
     private UserService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id){
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
 
-        return ResponseEntity.ok().body(service.findById(id));
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
     }
 }
