2f3eecb82d3618a5c9c2eeef54d68f05b3b5317c
vitoriachacon <vitoria.chacon@hotmail.com>
2024-10-19 19:39:36 -0300

    Teste para classe principal

--- src/test/java/br/com/chacon/ApiTestsJunitMockitoApplicationTests.java
+++ src/test/java/br/com/chacon/ApiTestsJunitMockitoApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class ApiTestsJunitMockitoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		ApiTestsJunitMockitoApplication.main(new String[] {});
 	}
-
 }
