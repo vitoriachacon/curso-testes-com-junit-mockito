ef4157a10d4e181ef7d15f74845e8035ecf58229
vitoriachacon <vitoria.chacon@hotmail.com>
2024-10-19 20:15:41 -0300

    Teste para classe UserService metodo findById

---
+++ src/test/java/br/com/chacon/services/impl/UserServiceImplTest.java
@@ -0,0 +1,86 @@
+package br.com.chacon.services.impl;
+
+import br.com.chacon.domain.User;
+import br.com.chacon.domain.dto.UserDTO;
+import br.com.chacon.repositories.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class UserServiceImplTest {
+
+    public static final Integer ID      = 1;
+    public static final String NAME     = "Vitoria";
+    public static final String EMAIL    = "vitoria@mail.com";
+    public static final String PASSWORD = "1234";
+
+    @InjectMocks /*A anotação @InjectMocks do Mockito é utilizada para criar e injetar mocks automaticamente no
+    objeto que está sendo testado. Quando você anota uma classe com @InjectMocks, o Mockito cria uma instância dessa
+    classe e injeta nela os mocks necessários (marcados com @Mock).*/
+    private UserServiceImpl service;
+
+    @Mock
+    private UserRepository repository;
+
+    @Mock
+    private ModelMapper mapper;
+
+    private User user;
+
+    private UserDTO userDTO;
+
+    private Optional<User> optionalUser;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startUser();
+    }
+
+    @Test
+    void whenFindByIdThenReturnAnUserInstance() {
+        when(repository.findById(anyInt())).thenReturn(optionalUser);
+
+        User response = service.findById(ID);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    private void startUser(){
+        user = new User(ID, NAME, EMAIL, PASSWORD);
+        userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
+        optionalUser = Optional.of(new User(ID, NAME, EMAIL, PASSWORD));
+    }
+}
