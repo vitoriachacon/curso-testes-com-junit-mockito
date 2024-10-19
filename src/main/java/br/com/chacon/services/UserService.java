1ce1897ff13b3c9d214bf573f3e12da016599f40
vitoriachacon <vitoria.chacon@hotmail.com>
2024-10-18 22:45:15 -0300

    Endpoint findAll para user

--- src/main/java/br/com/chacon/services/UserService.java
+++ src/main/java/br/com/chacon/services/UserService.java
@@ -2,7 +2,11 @@ package br.com.chacon.services;
 
 import br.com.chacon.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+
+    List<User> findAll();
 }
