cadafdf221cc5385ed007bfb6519dcbd95e7cb07
vitoriachacon <vitoria.chacon@hotmail.com>
2024-10-18 22:20:25 -0300

    Tratamento de exceção para objeto nao encontrado

--- src/main/java/br/com/chacon/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/chacon/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package br.com.chacon.services.impl;
 import br.com.chacon.domain.User;
 import br.com.chacon.repositories.UserRepository;
 import br.com.chacon.services.UserService;
+import br.com.chacon.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
