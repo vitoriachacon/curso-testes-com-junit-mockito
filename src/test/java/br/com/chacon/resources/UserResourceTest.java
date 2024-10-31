79873c5019db2138780742c335916abd641660c6
vitoriachacon <vitoria.chacon@hotmail.com>
2024-10-31 19:06:39 -0300

    Classe de testes para user resource iniciada

---
+++ src/test/java/br/com/chacon/resources/UserResourceTest.java
@@ -0,0 +1,65 @@
+package br.com.chacon.resources;
+
+import br.com.chacon.domain.User;
+import br.com.chacon.domain.dto.UserDTO;
+import br.com.chacon.services.impl.UserServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserResourceTest {
+
+    @InjectMocks
+    private UserResource resource;
+
+    @Mock
+    private UserServiceImpl service;
+
+    @Mock
+    private ModelMapper mapper;
+
+    private User user;
+
+    private UserDTO userDTO;
+
+    private static final Integer ID      = 1;
+    private static final String NAME     = "Vitoria";
+    private static final String EMAIL    = "vitoria@mail.com";
+    private static final String PASSWORD = "1234";
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startUser();
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    private void startUser(){
+        user = new User(ID, NAME, EMAIL, PASSWORD);
+        userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
+    }
+}
